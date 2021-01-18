aa23ad386aaa6d69cfcf63358e83f91bee6b2520
Ankit Choudhary <ankit.ch01@gmail.com>
2021-01-18 14:05:26 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+﻿var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/main.ts'),
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:63996',
+                secure: false
+            }
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'wwwroot'),
+        filename: 'app.[hash].js'
+    },
+    module: {
+        rules: [
+            { test: /\.component.ts$/, loaders: 'angular2-template-loader' },
+            { test: /\.ts$/, loaders: 'awesome-typescript-loader' },
+            { test: /\.html$/, loaders: 'html-loader' },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.ts', '.html', '.css']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin("styles.css")
+    ]
+};
